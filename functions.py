e592b600952da4259f4492e57a0aedb38d192b12
Gianni-De-vadder <103252946+Gianni-De-vadder@users.noreply.github.com>
2022-04-29 10:33:20 +0200

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,14 +1,32 @@
+from unicodedata import category
 import requests
 import bs4
 from bs4 import BeautifulSoup
-url = 'http://books.toscrape.com'
-page = requests.get(url)
+import csv
+main_url = 'http://books.toscrape.com/'
 links = []
-  
+url = '0'
 
-def lien(area,what):
+
+def Get_Links(area, what, url, search):
+		mycategory = []
+		page = requests.get(url)
 		soup = BeautifulSoup(page.text, "html.parser")
 		where = soup.find_all(area)
 		for i in where:
-				links = 'http://books.toscrape.com/' + (i[what])
-				print(links)
+				print(where)
+				if area=='a' or area=="img":
+					temp = main_url + (i[what])
+					if not search:
+						resultat = temp.__contains__(search)
+						if resultat:
+							mycategory.append(temp)	
+					else:
+						mycategory.append(temp)						
+				else:
+					links = what
+		return(mycategory)
+						
+def Navigation(link):
+	for i in link:
+		test = Get_Links('a', 'title', i, '')
