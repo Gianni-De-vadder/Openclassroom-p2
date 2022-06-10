339da1d0372cb13012a8944bc996c1492cc15cd3
Gianni De Vadder <gdevadder@lex-persona.com>
2022-06-10 10:13:25 +0200

    Adding 'get_books_image' fonction and docstring on functions

--- scrap.py
+++ scrap.py
@@ -1,18 +1,30 @@
-from utils.functions import get_categories, get_books_urls, get_book_datas
+from slugify import slugify
+from utils.functions import get_categories, get_books_urls, get_book_datas,get_categorie_name,get_book_image
+from pathlib import Path 
+
+
+DATA_DIR = "data/"
 
 
 def main():
-    
+    Path(DATA_DIR).mkdir(parents=True, exist_ok=True)
+    print('Récupération des catégories en cours...')
     categories_urls = get_categories('a' , 'href', 'http://books.toscrape.com/index.html','category')
-
     for url in categories_urls:
+        print(url)
+        category_name = get_categorie_name(url)
+        Path(DATA_DIR + category_name).mkdir(parents=True, exist_ok=True)
+        print(f'Traitement de la catégorie {category_name}')
         books_urls = get_books_urls(url)
-    
         books_data = []
+        
         for book_url in books_urls:
             book_data = get_book_datas(book_url)
-            books_data.append(book_data)
+            get_book_image(book_data["image_url"],category_name,slugify(book_data["title"]))
+            books_data.append(book_data)            
+            
+        
+        #save_data_to_csv
     
-    print(books_data)
 if __name__ == '__main__':
-    main()
+    main()    
--- utils/functions.py
+++ utils/functions.py
@@ -1,14 +1,16 @@
-from asyncore import write
+from os import mkdir
 from sys import exit
+from unicodedata import name
 import requests
 import bs4
 from bs4 import BeautifulSoup
-#from slugify import slugify
+from slugify import slugify
 import csv
-import urllib.request
-
-
+from pathlib import Path 
 MAIN_URL = 'http://books.toscrape.com/'
+DATA_DIR = "data/"
+
+print('coucou')
 
 def get_soup(url):
 	response = requests.get(url)
@@ -22,7 +24,15 @@ def get_soup(url):
 #-------------------------------------------------------------------------------------------------------------
 #								Récupération des catégories
 def get_categories(area, what, url, search=None):
-	"""Return list of categories urls"""
+	"""
+	It takes a url, and returns a list of all the links on that page
+	
+	:param area: the area where the links are located
+	:param what: the attribute of the tag you want to get
+	:param url: the url of the page you want to scrape
+	:param search: is the search term you want to use to filter the results
+	:return: A list of categories
+	"""
 	categories = []
 	soup = get_soup(url)
 	where = soup.find_all(area)
@@ -34,22 +44,36 @@ def get_categories(area, what, url, search=None):
 			else:	
 				resultat = temp.__contains__(search)	
 				if resultat:
-					categories.append(temp)			
+					categories.append(temp)		
 		else:
 			links = what
-	categories.pop(0)			
-	return(categories)
+	categories.pop(0)
+	return categories
+
+def get_categorie_name(url: str) -> str:
+    # Extract the last part of the url containing the category name beginning at 51
+    categorie_name = url[51:]
+    # Find the position of the "_" char
+    under_position = categorie_name.index("_")
+    categorie_name = categorie_name[:under_position]
+
+    return categorie_name
 
 #-------------------------------------------------------------------------------------------------------------
 #								Récupération des Livres de la catégorie
 def get_books_urls(url_category):
+	"""
+	It takes a url as an argument, and returns a list of urls for each book in that category
+	
+	:param url_category: the url of the category page
+	:return: A list of urls for each book in the category.
+	"""
 	books_urls = []
 	is_nextpage = True
 	next_url = url_category
 
 	while is_nextpage:	
 		#for url in url_category:
-		print(url_category)
 		page = get_soup(next_url)
 		h3_elements = page.find_all('h3')
 		for h3 in h3_elements:
@@ -60,7 +84,7 @@ def get_books_urls(url_category):
 		if btn_next:
 			url_btn_next = btn_next.a['href']
 			next_url = url_category.replace('index.html', url_btn_next)
-			print(next_url)
+			
 		else:
 			is_nextpage = False	
 
@@ -69,6 +93,12 @@ def get_books_urls(url_category):
 	
 
 def get_book_datas(book_url):
+	"""
+	It takes a URL, scrapes the page, and returns a dictionary of the data
+	
+	:param book_url: The URL of the book page
+	:return: A dictionary with the data of the book.
+	"""
 	"""Return a dict with data book """
 	url = book_url
 
@@ -155,14 +185,40 @@ def get_book_datas(book_url):
 		product_informations["number_available"] = availability
 	else:
 		product_informations["number_available"] = 0
-
+	
 	return product_informations
 
+def get_book_image(image_url,category_name,image_name):
+	"""
+	It takes an image url, a category name and an image name as parameters, and downloads the image to
+	the specified directory
+	
+	:param image_url: The URL of the image we want to download
+	:param category_name: The name of the category of the book
+	:param image_name: The name of the image file
+	"""
+	reponse = requests.get(image_url)
+	if not reponse.ok:
+		print("Il y a eu un problème lors de la récupération de l'image, passage à l'image suivante...")
+
+	else:
+		Path(DATA_DIR + category_name + "/images/").mkdir(parents=True, exist_ok=True)
+		with open(DATA_DIR + category_name + "/" + "images/" + image_name, "wb") as f:
+			f.write(reponse.content)
+
+#def savetocsv()
+    
 
 
 #if __name__ == "__main__":
 #csv.DictWriter
+# with open('test.csv', 'w', newline='') as csvfile:
+#     header = books_data[0].keys()
+#     fieldnames = ['first_name', 'last_name']
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
 
 # with open(monfichier.jpg, wb) as f:
 # 		write(datas)
+
+#header = books_data[0].keys()
