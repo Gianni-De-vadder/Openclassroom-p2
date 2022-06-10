aacbb3a97e740dc2101258189f4f94e8168ab137
Gianni De Vadder <gdevadder@lex-persona.com>
2022-06-10 14:12:00 +0200

    Adding SaveToCSV function, and rich library

--- scrap.py
+++ scrap.py
@@ -1,6 +1,8 @@
+from turtle import title
 from slugify import slugify
-from utils.functions import get_categories, get_books_urls, get_book_datas,get_categorie_name,get_book_image
+from utils.functions import get_categories, get_books_urls, get_book_datas,get_categorie_name,get_book_image,savetocsv
 from pathlib import Path 
+from rich.progress import track
 
 
 DATA_DIR = "data/"
@@ -18,10 +20,15 @@ def main():
         books_urls = get_books_urls(url)
         books_data = []
         
-        for book_url in books_urls:
+        for book_url,track(description= 'Processing' refresh_per_second=5) in books_urls:
             book_data = get_book_datas(book_url)
-            get_book_image(book_data["image_url"],category_name,slugify(book_data["title"]))
-            books_data.append(book_data)            
+            image_url = book_data['image_url']
+            book_title = slugify(book_data["title"])
+            get_book_image(image_url,category_name,book_title)
+            books_data.append(book_data)     
+            
+        savetocsv(category_name,books_data)
+            
             
         
         #save_data_to_csv
--- utils/functions.py
+++ utils/functions.py
@@ -1,6 +1,8 @@
 from os import mkdir
 from sys import exit
-from unicodedata import name
+from tkinter import image_names
+from turtle import title
+from unicodedata import category, name
 import requests
 import bs4
 from bs4 import BeautifulSoup
@@ -10,7 +12,6 @@ from pathlib import Path
 MAIN_URL = 'http://books.toscrape.com/'
 DATA_DIR = "data/"
 
-print('coucou')
 
 def get_soup(url):
 	response = requests.get(url)
@@ -18,7 +19,7 @@ def get_soup(url):
 		print(f"Un problème est survenu durant la requete avec l'url : {url}")
 		exit()
 
-	soup = BeautifulSoup(response.text, "html.parser")
+	soup = BeautifulSoup(response.content, "html.parser")
 	return soup	
 	
 #-------------------------------------------------------------------------------------------------------------
@@ -99,94 +100,76 @@ def get_book_datas(book_url):
 	:param book_url: The URL of the book page
 	:return: A dictionary with the data of the book.
 	"""
-	"""Return a dict with data book """
-	url = book_url
 
-	product_informations = { "product_page_url": url }
-
-	response = requests.get(product_informations["product_page_url"])
-
-	soup = BeautifulSoup(response.text, 'html.parser')
-
-	# Récupérer universal_product_code / price_excluding_taxe / price_including_tax
+	soup = get_soup(book_url)
+ 
+	tds = soup.find_all("td")
+ 
+	title = soup.find('h1').text
 	
-	informations = soup.findAll("tr")
-
-	for information in informations:
-		information_label = information.find('th').text
-		information_value = information.find('td').text
-
-		target_dict = False
-
-		if (information_label == "UPC"):
-			target_dict = "universal_product_code"
-		elif (information_label == "Price (excl. tax)"):
-			target_dict = "price_excluding_tax"
-		elif (information_label == "Price (incl. tax)"):
-			target_dict = "price_including_tax"
-
-		if target_dict:
-			if "Â" in information_value:
-				information_value = information_value.replace("Â", "")
-
-			product_informations[target_dict] = information_value
-
-		# Récupérer image_url (id product_gallery)
-		product_gallery = soup.find("div", {"id": "product_gallery"})
-		product_informations["image_url"] = "http://books.toscrape.com/" + \
-			product_gallery.find('img')["src"].replace('../../', '')
+	product_gallery = soup.find("div", {"id": "product_gallery"})
+	image_url = "http://books.toscrape.com/" + \
+	product_gallery.find('img')["src"].replace('../../', '')
 
 # Récupérer category (breadcrumbs : dernier li avant class active)
-		breadcrumb = soup.find('ul', {"class": "breadcrumb"})
-		links = breadcrumb.select('li:not(.active)')
-		product_informations["category"] = links[len(links) - 1].text.strip()
-
-# Récupérer title (titre H1)
-		product_informations['title'] = soup.find('h1').text
+	breadcrumb = soup.find('ul', {"class": "breadcrumb"})
+	links = breadcrumb.select('li:not(.active)')
+	category_name = links[len(links) - 1].text.strip()
 
 # Récupérer description (id product_description +  p)
-		description = soup.find('div', {"id": 'product_description'})
+	description = soup.find('div', {"id": 'product_description'})
 
-		if description:
-			product_informations["product_description"] = description.findNext('p').text
+	if description:
+		description = description.findNext('p').text
+	else:
+		print(f" Livre sans description : {book_url}")
+		description = "Ce livre n'a pas de description"
 
 # Récupérer review_rating (class star-rating + class indiquant le nombre d'étoile)
-		review_rating = soup.find('p', {"class": "star-rating"})
-		if review_rating.has_attr('class'):
-			review_rating = review_rating["class"][1]
-
-		if review_rating == "One":
-			review_rating = 1
-		elif review_rating == "Two":
-			review_rating = 2
-		elif review_rating == "Three":
-			review_rating = 3
-		elif review_rating == "Four":
-			review_rating = 4
-		elif review_rating == "Five":
-			review_rating = 5
-		else:
-			review_rating = 0
-	
-	else:
-		review_rating = 0
-
-	product_informations['review_rating'] = review_rating
+	review_rating = soup.find('p', {"class": "star-rating"})
+	review_rating = review_rating["class"][1]
+	review_rating = transform_rating(review_rating)
 
 # Récupérer number_available (instock outofstock en dessous du prix du produit)
-	availability = soup.select('p.availability.instock')
-
-	if availability:
-		availability = availability[0].text
-		availability = availability.replace('In stock (', '')
-		availability = availability.replace(' available)', '')
-		availability = int(availability)
-
-		product_informations["number_available"] = availability
-	else:
-		product_informations["number_available"] = 0
+	availability = tds[5].text[10:]
+	availability = availability.replace(' available)', '')
+	availability = int(availability)
+ 
 	
+	
+	category_name = slugify(category_name)
+	image_file =  f"{DATA_DIR}{category_name}/images/{slugify(title)}.jpg"
+ 
+ 
+	product_informations = {}
+	product_informations['title'] = title
+	product_informations["product_page_url"] =  book_url
+	product_informations["category"] = category_name
+	product_informations["product_description"] = description.strip()
+	product_informations["image_url"] = image_url
+	product_informations['image_file'] = image_file
+	product_informations["universal_product_code"] = tds[0].text
+	product_informations["price_excluding_tax"] = tds[2].text
+	product_informations["price_including_tax"] = tds[3].text
+	product_informations['review_rating'] = review_rating
+	product_informations["number_available"] = availability
+ 
 	return product_informations
+
+def transform_rating(rating_text):
+    rating = 0
+    if rating_text == "One":
+        rating = 1
+    elif rating_text == "Two":
+        rating = 2
+    elif rating_text == 'Three':
+        rating = 3
+    elif rating_text == "Four":
+        rating = 4
+    elif rating_text == "Five":
+        rating = 5
+    
+    return rating
 
 def get_book_image(image_url,category_name,image_name):
 	"""
@@ -203,13 +186,26 @@ def get_book_image(image_url,category_name,image_name):
 
 	else:
 		Path(DATA_DIR + category_name + "/images/").mkdir(parents=True, exist_ok=True)
-		with open(DATA_DIR + category_name + "/" + "images/" + image_name, "wb") as f:
+		with open(f"{DATA_DIR}{category_name}/images/{image_name}.jpg", "wb") as f:
 			f.write(reponse.content)
 
-#def savetocsv()
-    
 
 
+def savetocsv(category_name,books_data):
+	"""
+	It takes a category name and a list of dictionaries as input, and writes the list of dictionaries to
+	a csv file
+	
+	:param category_name: The name of the category you want to save the data to
+	:param books_data: a list of dictionaries, each dictionary is a book's data
+	"""
+	field_names = books_data[0].keys()
+	category_path = DATA_DIR + category_name + "/" 
+	csv_name = category_name +  ".csv"
+	with open(category_path + csv_name , 'w', newline='', encoding='utf-8-sig') as csvfile:
+		w = csv.DictWriter(csvfile, fieldnames=field_names)
+		w.writeheader()
+		w.writerows(books_data)
 #if __name__ == "__main__":
 #csv.DictWriter
 # with open('test.csv', 'w', newline='') as csvfile:
